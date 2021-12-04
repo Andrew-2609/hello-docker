ac3606ccf9e05f093bea1f58eb40b99bc0765f37
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-03 21:17:37 -0300

    Feat: add a new endpoint to return an instance of HelloDocker

--- src/main/java/com/ndrewcoding/hellodocker/HelloDockerController.java
+++ src/main/java/com/ndrewcoding/hellodocker/HelloDockerController.java
@@ -1,2 +1,18 @@
-package com.ndrewcoding.hellodocker;public class HelloDockerController {
+package com.ndrewcoding.hellodocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-docker")
+public class HelloDockerController {
+
+    @GetMapping
+    public HelloDocker greeting() {
+        String hostName = System.getenv("HOSTNAME");
+
+        return new HelloDocker("Hello, Docker!", hostName);
+    }
+
 }
