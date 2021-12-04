4d2b7a7282e71ed8e993b3636dd8ccf497710023
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-03 21:13:24 -0300

    Feat: add basic implementation of HelloDocker.java

--- src/main/java/com/ndrewcoding/hellodocker/HelloDocker.java
+++ src/main/java/com/ndrewcoding/hellodocker/HelloDocker.java
@@ -1,2 +1,19 @@
-package com.ndrewcoding.hellodocker;public class HelloDocker {
+package com.ndrewcoding.hellodocker;
+
+public class HelloDocker {
+    private final String content;
+    private final String environment;
+
+    HelloDocker(String content, String environment) {
+        this.content = content;
+        this.environment = environment;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
 }
